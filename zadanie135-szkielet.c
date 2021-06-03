1c946a8a7dd0d3ffa8707b98f52397efec946688
Patryk Brzychcy <patryk.brzychcy@gmail.com>
2021-06-03 08:12:50 +0200

    dodanie pola trojkata

--- zadanie135-szkielet.c
+++ zadanie135-szkielet.c
@@ -56,7 +56,40 @@ void prostokat() {
 
 void trojkat() {
     /* TODO - osoba3 */
+float a, h, wynik;
+    printf("podaj a: \n");
+    if(scanf("%f", &a)==0)
+        {
+        printf("podaj cyfre\n");
+        exit(EXIT_FAILURE);
+        }
+    else
+    {
+    if(a<=0)
+    {
+
+        printf("podaj a wieksze od 0\n");
+        exit(EXIT_FAILURE);
+    }
+    }
+    printf("podaj h: \n");
+    if(scanf("%f", &h)==0)
+        {
+        printf("podaj cyfre");
+        exit(EXIT_FAILURE);
+        }
+    else
+    {
+    if(h<=0)
+    {
+
+        printf("podaj h wieksze od 0\n");
+        exit(EXIT_FAILURE);
+    }
+    }
+    wynik= ((a * h)/2.0);
+
 
     /* TODO - właściwe obliczanie pola zrealizować poprzez dedykowaną funkcję */
-    printf("\npole trójk¹ta - TODO\n\n");
+    printf("%s\n" "%.2f" "%s", "pole trojkata - ", wynik, "\n\n" );
 }
